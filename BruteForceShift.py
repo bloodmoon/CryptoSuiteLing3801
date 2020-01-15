c0b0470632de24201e37e199b07148746ba10f67
batey.18 <batey.18@osu.edu>
2020-01-15 10:05:43 -0500

    Comments

--- BruteForceShift.py
+++ BruteForceShift.py
@@ -1,29 +1,46 @@
-from ShiftDecoder import ShiftDecode
-shiftList = list(range(26))
-inputFile = open("cipherText.txt")
-outputFile = open("plainText.txt", "w")
+"""
+Author: Brian Batey
 
+A simple program to brute force
+a Shift Cipher that utilizes a non-random (consecutive) character shift.
+
+Takes a text file as input for the cipher text and outputs a text file
+containing every consecutive alphabetical shift in the range of (0-25)
+"""
+from ShiftDecoder import ShiftDecode
+
+# Setup Variables
+shiftList = list(range(26))
 cipherText = []
 
+# Setup File I/O and get user response
+inputFile = open(input("Please enter the path/name of the cipher text file: "))
+outputFile = open(input("Please enter the path/name of the desired output file: "), "w")
+
+
+# Read characters into cipherText list from input file
 while True:
     currentChar = inputFile.read(1)
-    if currentChar is '':
+    if currentChar is '':  # Check if EOF is reached
         break
     else:
         cipherText.append(currentChar)
 
+# Shift each character using ShiftDecode object and output result to file
 for element in shiftList:
     outputFile.write("\n")
     outputFile.write(str(element))
     outputFile.write("\n")
 
-    test = ShiftDecode(element)
+    # Shift current character from "element" and file writing / formatting
+    shiftedChar = ShiftDecode(element)
     for ele in cipherText:
         if ord(ele) is 10:
             outputFile.write("\n")
         elif ele is not ' ':
-            outputFile.write(test.decode_char(ele).lower())
+            outputFile.write(shiftedChar.decode_char(ele).lower())
     outputFile.write("\n")
 
+# Cleanup
 inputFile.close()
 outputFile.close()
